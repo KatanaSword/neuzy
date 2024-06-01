fd56b1d09a0e8a3e182920eaadb84965598bcf16
KatanaSword <114555620+KatanaSword@users.noreply.github.com>
2024-06-01 13:51:17 +0530

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+const app = express();
+
+dotenv.config({
+  path: "./.env",
+});
+
+connectDB()
+  .then(() => {
+    app.on("Error", (error) => {
+      console.log("Error:", error);
+    });
+    app.listen(process.env.PORT || 3500, () => {
+      console.log(`⚙️  Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed !!!", error);
+  });
