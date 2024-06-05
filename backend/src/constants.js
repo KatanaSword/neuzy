bde9278e975948811edf610ca5a02b53a747468c
Saurabh Tajane <114555620+KatanaSword@users.noreply.github.com>
2024-06-05 13:40:23 +0530

    Added user role object and USER_TEMPORARY_TOKEN_EXPIRY

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -1,1 +1,12 @@
 export const DB_NAME = "Neuzy";
+
+export const userRole = {
+  USER: "USER",
+  SUBSCRIBER: "SUBSCRIBER",
+  AUTHOR: AUTHOR,
+  ADMIN: "ADMIN",
+};
+
+export const availableUserRole = Object.values(userRole);
+
+export const USER_TEMPORARY_TOKEN_EXPIRY = 10 * 60 * 1000;
