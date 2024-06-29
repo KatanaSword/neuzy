cf08f153235e2b9818d7e31a094380d819f38576
Saurabh Tajane <114555620+KatanaSword@users.noreply.github.com>
2024-06-30 00:06:10 +0530

    Add file upload and remove functionality in cloud using cloudinary

---
+++ backend/src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath, cloudinaryFolder) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "image",
+      folder: cloudinaryFolder,
+    });
+    fs.unlinkSync(localPath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localPath);
+    return null;
+  }
+};
+
+const deleteFromCloudinary = async (publicId) => {
+  if (!publicId) return null;
+  await cloudinary.uploader.destroy(publicId);
+};
+
+export { uploadOnCloudinary, deleteFromCloudinary };
