0208551f9971a688ed3b403ba20f5c5b7ea64a24
Saurabh Tajane <114555620+KatanaSword@users.noreply.github.com>
2024-07-05 16:03:30 +0530

    Add Mongoose pagination options helper function

---
+++ backend/src/utils/helpers.js
@@ -0,0 +1,14 @@
+export const getMongoosePaginationOptions = ({
+  page = 1,
+  limit = 10,
+  customLabels,
+}) => {
+  return {
+    page: Math.max((page = 1)),
+    limit: Math.max((limit = 1)),
+    customLabels: {
+      pagingCounter: "SerialNumberStartFrom",
+      ...customLabels,
+    },
+  };
+};
