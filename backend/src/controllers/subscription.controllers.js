af80bf59a40c316d08fbd5d8d95078830ad1d458
Saurabh Tajane <114555620+KatanaSword@users.noreply.github.com>
2024-07-21 14:17:24 +0530

    Add verifyRazorpayPayment functionality

--- backend/src/controllers/subscription.controllers.js
+++ backend/src/controllers/subscription.controllers.js
@@ -5,7 +5,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { nanoid } from "nanoid";
 import { Subscription } from "../models/subscription.models.js";
 import { paymentMethod, userSubscription } from "../constants.js";
-import { sendEmail } from "../utils/mail.js";
+import crypto from "crypto";
 
 let razorpayInstance;
 try {
@@ -86,5 +86,30 @@ const generateRazorpayOrder = asyncHandler(async (req, res) => {
     }
   );
 });
+
+const verifyRazorpayPayment = asyncHandler(async (req, res) => {
+  const { razorpay_payment_id, razorpay_order_id, razorpay_signature } =
+    req.body;
+
+  let body = razorpay_order_id + "|" + razorpay_payment_id;
+
+  const expectedSignature = crypto
+    .createHmac("sha256", process.env.KEY_SECRET)
+    .update(body.toString())
+    .digest("hex");
+
+  if (expectedSignature === razorpay_signature) {
+    const subscription = await orderFulfillmentHelper(razorpay_payment_id);
+    return res
+      .status(200)
+      .json(new ApiResponse(201, subscription, "Order place successfully"));
+  } else {
+    throw new ApiError(400, "Invalid razorpay signature");
+  }
+});
+
+const getSubscriptionById = asyncHandler(async (req, res) => {});
+
+const getSubscriberListAdmin = asyncHandler(async (req, res) => {});
 
 export { generateRazorpayOrder };
