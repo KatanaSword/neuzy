154ed8fb84a9ca896b41b7fd2e9a94b287891c83
Saurabh Tajane <114555620+KatanaSword@users.noreply.github.com>
2024-07-21 11:47:25 +0530

    Declear subscription router

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,11 +21,13 @@ import userRouter from "./routers/user.routers.js";
 import categoryRouter from "./routers/category.routers.js";
 import articleRouter from "./routers/article.routers.js";
 import likeRouter from "./routers/like.routers.js";
+import subscriptionRouter from "./routers/subscription.routers.js";
 
 // Declear router
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/categories", categoryRouter);
 app.use("/api/v1/articles", articleRouter);
 app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 export { app };
